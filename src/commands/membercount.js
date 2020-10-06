eeb7e8bcdb933094227111e915ae2ab737903fbb
Hriday Sharma <hridaysharma42@gmail.com>
2020-10-07 01:27:52 +0530

    commands/membercount.js: Change Indent to tabs

--- src/commands/membercount.js
+++ src/commands/membercount.js
@@ -1,17 +1,17 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'membercount',
-    func: function (message) {
-        message.channel.send(
-            new MessageEmbed()
-                .setAuthor('Member Count', message.guild.iconURL())
-                .setColor('GREEN')
-                .addFields(
-                    { name: 'Members', value: message.guild.memberCount },
-                    { name: 'Humans', value: message.guild.members.cache.filter(member => !member.user.bot).size },
-                    { name: 'Bots', value: message.guild.members.cache.filter(member => member.user.bot).size }
-                )
-        );
-    }
+	name: 'membercount',
+	func: function (message) {
+		message.channel.send(
+			new MessageEmbed()
+				.setAuthor('Member Count', message.guild.iconURL())
+				.setColor('GREEN')
+				.addFields(
+					{ name: 'Members', value: message.guild.memberCount },
+					{ name: 'Humans', value: message.guild.members.cache.filter(member => !member.user.bot).size },
+					{ name: 'Bots', value: message.guild.members.cache.filter(member => member.user.bot).size }
+				)
+		);
+	}
 };
