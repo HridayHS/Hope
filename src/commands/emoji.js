1503d404bc2e248028236f10ce4e04597bd72b5b
Hriday Sharma <hridaysharma42@gmail.com>
2020-10-12 16:46:25 +0530

    commands/emoji: Convert indentation to tabs

--- src/commands/emoji.js
+++ src/commands/emoji.js
@@ -1,23 +1,23 @@
 module.exports = {
-    name: 'emoji',
-    alias: ['e', 'emote', 'emotelink', 'emojilink', 'elink'],
-    func: async function (message) {
+	name: 'emoji',
+	alias: ['e', 'emote', 'emotelink', 'emojilink', 'elink'],
+	func: async function (message) {
 
-        const doesMessageContainCustomEmoji = message.content.split(' ')[2].match(/^(:[^:\s]+:|<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>)+$/g);
+		const doesMessageContainCustomEmoji = message.content.split(' ')[2].match(/^(:[^:\s]+:|<:[^:\s]+:[0-9]+>|<a:[^:\s]+:[0-9]+>)+$/g);
 
-        if (!doesMessageContainCustomEmoji) {
-            message.channel.send('Invalid emoji.');
-            return;
-        }
+		if (!doesMessageContainCustomEmoji) {
+			message.channel.send('Invalid emoji.');
+			return;
+		}
 
-        const emoji = {
-            info: doesMessageContainCustomEmoji[0].slice(1).slice(0, -1).split(':')
-        };
+		const emoji = {
+			info: doesMessageContainCustomEmoji[0].slice(1).slice(0, -1).split(':')
+		};
 
-        emoji.id = emoji.info[2];
-        emoji.extention = emoji.info.includes('a') ? 'gif' : 'png';
-        emoji.url = `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.extention}`;
+		emoji.id = emoji.info[2];
+		emoji.extention = emoji.info.includes('a') ? 'gif' : 'png';
+		emoji.url = `https://cdn.discordapp.com/emojis/${emoji.id}.${emoji.extention}`;
 
-        message.channel.send(`<${emoji.url}>`, { files: [emoji.url] });
-    }
+		message.channel.send(`<${emoji.url}>`, { files: [emoji.url] });
+	}
 };
