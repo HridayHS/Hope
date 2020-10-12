d7d0a168b13df5fae433d246a3360175179e6e38
Hriday Sharma <hridaysharma42@gmail.com>
2020-10-12 16:15:15 +0530

    commands/serverinfo: Convert indentation to tabs

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -1,45 +1,45 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'serverinfo',
-    guildOnly: true,
-    func: async function (message) {
-        const serverCreated = message.guild.createdAt;
-        const serverRegion = region => {
-            switch (region) {
-                case 'hongkong':
-                    return 'Hong Kong'
-                case 'southafrica':
-                    return 'South Africa'
-                case 'us-central':
-                    return 'US Central'
-                case 'us-east':
-                    return 'US East'
-                case 'us-south':
-                    return 'US South'
-                case 'us-west':
-                    return 'US West'
-                default:
-                    return region.charAt(0).toUpperCase() + region.slice(1);
-            }
-        };
-        const serverRoles = async () => {
-            let rolesList = '';
-            (await message.guild.roles.fetch()).cache.forEach(role => rolesList += role.name + ', ');
-            return rolesList;
-        };
+	name: 'serverinfo',
+	guildOnly: true,
+	func: async function (message) {
+		const serverCreated = message.guild.createdAt;
+		const serverRegion = region => {
+			switch (region) {
+				case 'hongkong':
+					return 'Hong Kong'
+				case 'southafrica':
+					return 'South Africa'
+				case 'us-central':
+					return 'US Central'
+				case 'us-east':
+					return 'US East'
+				case 'us-south':
+					return 'US South'
+				case 'us-west':
+					return 'US West'
+				default:
+					return region.charAt(0).toUpperCase() + region.slice(1);
+			}
+		};
+		const serverRoles = async () => {
+			let rolesList = '';
+			(await message.guild.roles.fetch()).cache.forEach(role => rolesList += role.name + ', ');
+			return rolesList;
+		};
 
-        message.channel.send(
-            new MessageEmbed()
-                .setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
-                .setColor('GREEN')
-                .addFields(
-                    { name: 'Owner', value: message.guild.owner, inline: true },
-                    { name: 'Region', value: serverRegion(message.guild.region), inline: true },
-                    { name: 'Roles', value: (await message.guild.roles.fetch()).cache.size, inline: true },
-                    { name: 'Roles List', value: await serverRoles(), inline: false },
-                )
-                .setFooter('ID: ' + message.guild.id + ' | ' + 'Server Created • ' + `${serverCreated.getDate()}/${serverCreated.getMonth()}/${serverCreated.getFullYear()}`)
-        );
-    }
+		message.channel.send(
+			new MessageEmbed()
+				.setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
+				.setColor('GREEN')
+				.addFields(
+					{ name: 'Owner', value: message.guild.owner, inline: true },
+					{ name: 'Region', value: serverRegion(message.guild.region), inline: true },
+					{ name: 'Roles', value: (await message.guild.roles.fetch()).cache.size, inline: true },
+					{ name: 'Roles List', value: await serverRoles(), inline: false },
+				)
+				.setFooter('ID: ' + message.guild.id + ' | ' + 'Server Created • ' + `${serverCreated.getDate()}/${serverCreated.getMonth()}/${serverCreated.getFullYear()}`)
+		);
+	}
 };
