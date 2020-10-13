f03d1e7c52642c305161bd8077e869fce9c8f090
Hriday Sharma <hridaysharma42@gmail.com>
2020-10-13 16:47:05 +0530

    commands/serverinfo: Visual update
    * Display guild icon as thumbnail as well.
    * Specify whose id we are showing in footer.

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -32,6 +32,7 @@ module.exports = {
 		message.channel.send(
 			new MessageEmbed()
 				.setAuthor(message.guild.name, message.guild.iconURL({ dynamic: true }))
+				.setThumbnail(message.guild.iconURL({ format: 'png', dynamic: true, size: 4096 }))
 				.setColor('GREEN')
 				.addFields(
 					{ name: 'Owner', value: message.guild.owner, inline: true },
@@ -39,7 +40,7 @@ module.exports = {
 					{ name: 'Roles', value: (await message.guild.roles.fetch()).cache.size, inline: true },
 					{ name: 'Roles List', value: await serverRoles(), inline: false },
 				)
-				.setFooter('ID: ' + message.guild.id + ' | ' + 'Server Created • ' + `${serverCreated.getDate()}/${serverCreated.getMonth()}/${serverCreated.getFullYear()}`)
+				.setFooter('Server ID: ' + message.guild.id + ' | ' + 'Server Created • ' + `${serverCreated.getDate()}/${serverCreated.getMonth()}/${serverCreated.getFullYear()}`)
 		);
 	}
 };
