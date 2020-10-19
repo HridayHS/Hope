e4bdc3a2c637e159e212b88ef12c978c97ca8265
Hriday Sharma <hridaysharma42@gmail.com>
2020-10-20 00:14:08 +0530

    commands/bot-star: version: Drop embed
    * Start version number with 'Version'.
    * Also change indentation to tabs.

--- src/commands/bot-star/version.js
+++ src/commands/bot-star/version.js
@@ -1,14 +1,9 @@
-const { MessageEmbed } = require('discord.js');
 const { botVersion } = require('../../../utils');
 
 module.exports = {
-    name: 'version',
-    alias: ['v'],
-    func: async function (message) {
-        message.channel.send(
-            new MessageEmbed()
-                .setColor('GREEN')
-                .setDescription(await botVersion())
-        );
-    }
+	name: 'version',
+	alias: ['v'],
+	func: async function (message) {
+		message.channel.send('Version ' + await botVersion());
+	}
 };
