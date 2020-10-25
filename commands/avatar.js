df8698b7be3f53ed7a61a8c37046eb9f416c6977
Hriday Sharma <hridaysharma42@gmail.com>
2020-10-25 22:51:12 +0530

    commands: avatar: Wrap up image in an embed
    * Set embed color to average color of an image.
      - Requires fast-average-color-node to do this.

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,3 +1,6 @@
+const { DataResolver } = require('discord.js');
+const { getAverageColor } = require('fast-average-color-node');
+
 module.exports = {
 	name: 'avatar',
 	alias: ['av'],
@@ -12,10 +15,27 @@ module.exports = {
 		};
 
 		const user = await getUser();
-		if (user) {
-			message.channel.send(user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 }));
-		} else {
+
+		if (!user) {
 			message.channel.send(`Unable to find user.`);
+			return;
 		}
+
+		const userAvatar = user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
+		const avatarBuffer = await DataResolver.resolveFileAsBuffer(userAvatar);
+		const avatarAverageColor = (await getAverageColor(avatarBuffer)).hex;
+
+		message.channel.send({
+			embed: {
+				color: avatarAverageColor,
+				author: {
+					name: user.tag,
+					icon_url: user.displayAvatarURL({ dynamic: true }),
+				},
+				image: {
+					url: userAvatar
+				}
+			}
+		});
 	}
 };
