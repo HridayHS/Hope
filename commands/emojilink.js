36bd19d50b85dc8bf68e11cc9136b5ca36b1b841
Hriday Sharma <hridaysharma42@gmail.com>
2020-11-07 00:30:29 +0530

    commands: emojilink: Code improvements

--- commands/emojilink.js
+++ commands/emojilink.js
@@ -1,3 +1,15 @@
+class Emoji {
+	constructor(info) {
+		this.info = info;
+	}
+
+	get url() {
+		const emojiID = this.info[2];
+		const emojiExtention = this.info.includes('a') ? 'gif' : 'png';
+		return `https://cdn.discordapp.com/emojis/${emojiID}.${emojiExtention}`;
+	}
+}
+
 module.exports = {
 	name: 'emojilink',
 	alias: ['e', 'emoji', 'emote', 'emotelink', 'elink'],
@@ -10,32 +22,29 @@ module.exports = {
 			return;
 		}
 
-		const Emoji = {
-			info: undefined,
-			get url() {
-				const emojiID = this.info[2];
-				const emojiExtention = this.info.includes('a') ? 'gif' : 'png';
-				return `https://cdn.discordapp.com/emojis/${emojiID}.${emojiExtention}`;
-			}
-		};
-
 		if (message.channel.type === 'dm') {
-			for (let i = 0; i < (Emojis.length > 5 ? 5 : Emojis.length); i++) {
-				Emoji.info = Emojis[i].slice(1).slice(0, -1).split(':');
+			for (let i = 0; i < Emojis.length; i++) {
+				if (i === 5) break; // Don't send emoji links more than 5.
+
+				const EmojiInfo = Emojis[i].slice(1).slice(0, -1).split(':');
+				const EmojiURL = new Emoji(EmojiInfo).url
+
 				message.channel.send(
-					isLinkNeeded ? `<${Emoji.url}>` : Emoji.url, // Message content
-					isLinkNeeded ? { files: [Emoji.url] } : null // Message options
+					isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
+					isLinkNeeded ? { files: [EmojiURL] } : null // Message options
 				);
 			}
 			return;
 		}
 
-		const canSendFiles = message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES');
-		Emoji.info = Emojis[0].slice(1).slice(0, -1).split(':');
+		const EmojiInfo = Emojis[0].slice(1).slice(0, -1).split(':');
+		const EmojiURL = new Emoji(EmojiInfo).url
+
+		const canBotSendFiles = message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES');
 
 		message.channel.send(
-			isLinkNeeded ? `<${Emoji.url}>` : Emoji.url, // Message content
-			(isLinkNeeded && canSendFiles) ? { files: [Emoji.url] } : null // Message options
+			isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
+			(isLinkNeeded && canBotSendFiles) ? { files: [EmojiURL] } : null // Message options
 		);
 	}
 };
