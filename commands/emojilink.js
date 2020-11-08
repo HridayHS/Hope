846a18df250495fc2e2d5f3becc36428d8b2be36
Hriday Sharma <hridaysharma42@gmail.com>
2020-11-09 04:50:21 +0530

    commands: emojilink: Add support for getting emojis from user custom status
    * With this commit PRESENCE INTENT needs to be turned on.
      So that bot can get user's presence to work with.

--- commands/emojilink.js
+++ commands/emojilink.js
@@ -10,45 +10,85 @@ class Emoji {
 	}
 }
 
+async function getUser(message) {
+	const userID = message.content.split(' ')[2];
+	const userMention = message.mentions.users.first();
+	return userMention ?? await message.client.users.fetch(userID, true, true).catch(() => {});
+};
+
 module.exports = {
 	name: 'emojilink',
 	alias: ['e', 'emoji', 'emote', 'emotelink', 'elink'],
-	func: function (message) {
+	func: async function (message) {
 		const Emojis = message.content.match(/(<:|<a:)+[^:]+:[0-9]+>/g);
+		const user = await getUser(message);
 
-		if (!Emojis) {
+		if (!Emojis && !user) {
 			message.channel.send('Invalid emoji.');
 			return;
 		}
 
+		const canBotSendFiles = (message.channel.type === 'dm') ? true
+			: message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES');
 		const isLinkNeeded = message.content.match('link');
+		const isMessageDeletionRequired = message.channel.type !== 'dm' && message.content.match('delete');
 
-		if (message.channel.type === 'dm') {
-			for (let i = 0; i < Emojis.length; i++) {
-				if (i === 5) break; // Don't send emoji links more than 5.
+		/* Custom emoji from message content. */
+		if (Emojis) {
+			// Delete user message if deletion is required.
+			if (isMessageDeletionRequired) {
+				message.delete();
+			}
 
+			// Send upto five Emojis for DMChannel and only one for other ones.
+			const amountOfEmojisToSend = (message.channel.type === 'dm') ? (Emojis.length > 5 ? 5 : Emojis.length) : 1;
+
+			for (let i = 0; i < amountOfEmojisToSend; i++) {
 				const EmojiURL = new Emoji(Emojis[i]).url;
-
 				message.channel.send(
 					isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
-					isLinkNeeded ? { files: [EmojiURL] } : null // Message options
+					(isLinkNeeded && canBotSendFiles) ? { files: [EmojiURL] } : null // Message options
 				);
 			}
 			return;
 		}
 
-		// Delete user message if deletion is required.
-		const isMessageDeletionRequired = message.content.match('delete');
-		if (isMessageDeletionRequired) {
-			message.delete();
+		/* Custom emoji from user custom status */
+		if (user) {
+			const userActivities = user.presence.activities;
+			if (userActivities.length === 0) {
+				message.channel.send('Unable to retrieve emoji.');
+				return;
+			}
+
+			for (let i = 0; i < userActivities.length; i++) {
+				const activity = userActivities[i];
+
+				// Skip if activity type isn't custom status.
+				if (activity.type !== 'CUSTOM_STATUS') continue;
+
+				const Emoji = activity.emoji;
+				if (!Emoji || !Emoji.id) {
+					message.channel.send('Custom emoji not found!');
+					return;
+				}
+
+				// Delete user message if deletion is required.
+				if (isMessageDeletionRequired) {
+					message.delete();
+				}
+
+				const EmojiID = Emoji.id;
+				const EmojiExtention = Emoji.animated ? 'gif' : 'png';
+				const EmojiURL = `https://cdn.discordapp.com/emojis/${EmojiID}.${EmojiExtention}`;
+
+				message.channel.send(
+					isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
+					(isLinkNeeded && canBotSendFiles) ? { files: [EmojiURL] } : null // Message options
+				);
+
+				return;
+			}
 		}
-
-		const canBotSendFiles = message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES');
-		const EmojiURL = new Emoji(Emojis[0]).url;
-
-		message.channel.send(
-			isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
-			(isLinkNeeded && canBotSendFiles) ? { files: [EmojiURL] } : null // Message options
-		);
 	}
 };
