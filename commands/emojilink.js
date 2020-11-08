0b1294260d259e934b35a39d095d224f9b7e3854
Hriday Sharma <hridaysharma42@gmail.com>
2020-11-09 00:26:25 +0530

    commands: emojilink: Improvements to class Emoji

--- commands/emojilink.js
+++ commands/emojilink.js
@@ -1,11 +1,11 @@
 class Emoji {
-	constructor(info) {
-		this.info = info;
+	constructor(emoji) {
+		this.emojiInfo = emoji.slice(1).slice(0, -1).split(':');
 	}
 
 	get url() {
-		const emojiID = this.info[2];
-		const emojiExtention = this.info.includes('a') ? 'gif' : 'png';
+		const emojiID = this.emojiInfo[2];
+		const emojiExtention = this.emojiInfo.includes('a') ? 'gif' : 'png';
 		return `https://cdn.discordapp.com/emojis/${emojiID}.${emojiExtention}`;
 	}
 }
@@ -15,19 +15,19 @@ module.exports = {
 	alias: ['e', 'emoji', 'emote', 'emotelink', 'elink'],
 	func: function (message) {
 		const Emojis = message.content.match(/(<:|<a:)+[^:]+:[0-9]+>/g);
-		const isLinkNeeded = message.content.match('link');
 
 		if (!Emojis) {
 			message.channel.send('Invalid emoji.');
 			return;
 		}
 
+		const isLinkNeeded = message.content.match('link');
+
 		if (message.channel.type === 'dm') {
 			for (let i = 0; i < Emojis.length; i++) {
 				if (i === 5) break; // Don't send emoji links more than 5.
 
-				const EmojiInfo = Emojis[i].slice(1).slice(0, -1).split(':');
-				const EmojiURL = new Emoji(EmojiInfo).url;
+				const EmojiURL = new Emoji(Emojis[i]).url;
 
 				message.channel.send(
 					isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
@@ -37,10 +37,8 @@ module.exports = {
 			return;
 		}
 
-		const EmojiInfo = Emojis[0].slice(1).slice(0, -1).split(':');
-		const EmojiURL = new Emoji(EmojiInfo).url;
-
 		const canBotSendFiles = message.channel.permissionsFor(message.guild.me).has('ATTACH_FILES');
+		const EmojiURL = new Emoji(Emojis[0]).url;
 
 		message.channel.send(
 			isLinkNeeded ? `<${EmojiURL}>` : EmojiURL, // Message content
