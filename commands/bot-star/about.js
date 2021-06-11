feb67e22ed0b03a6c667b89506033315a176b6fe
Hriday Sharma <hridaysharma42@gmail.com>
2021-06-12 03:35:39 +0530

    commands: about: Minor code improvemnts

--- commands/bot-star/about.js
+++ commands/bot-star/about.js
@@ -1,14 +1,16 @@
-const { botPermissions } = require('../../utils');
-const { customDateFormat } = require('../../utils');
+const { botPermissions, customDateFormat } = require('../../utils');
 
 module.exports = {
 	name: 'about',
 	alias: ['abt', 'bot', 'abtbot', 'aboutbout', 'star', 'abtstar', 'aboutstar'],
 	func: async function (message) {
 		const bot = await message.client.fetchApplication();
+
 		const botCreatedAt = customDateFormat(bot.createdAt);
+		const botDeveloper = bot.owner.owner.user.tag;
+		const botGuildsSize = message.client.guilds.cache.size;
 		const botInviteLink = await message.client.generateInvite({ permissions: botPermissions });
-		const teamMembersList = bot.owner.members.map(member => member.user.tag).sort().join(', ');
+		const botTeamMembersList = bot.owner.members.map(member => member.user.tag).sort().join(', ');
 
 		message.channel.send({
 			embed: {
@@ -19,9 +21,9 @@ module.exports = {
 				color: 'GREEN',
 				fields: [
 					{ name: 'Created on', value: botCreatedAt, inline: true },
-					{ name: 'Developer', value: bot.owner.owner.user.tag, inline: true },
-					{ name: 'Used in', value: `${message.client.guilds.cache.size} Servers`, inline: true },
-					{ name: 'Team members', value: teamMembersList, inline: false },
+					{ name: 'Developer', value: botDeveloper, inline: true },
+					{ name: 'Used in', value: `${botGuildsSize} Servers`, inline: true },
+					{ name: 'Team members', value: botTeamMembersList, inline: false },
 					{ name: 'Discord', value: '[Click here](https://discord.gg/cnkAxAT)', inline: true },
 					{ name: 'Invite link', value: `[Click here](${botInviteLink})`, inline: true },
 					{ name: 'Source code', value: '[GitHub](https://github.com/HridayHS/Star)', inline: true }
